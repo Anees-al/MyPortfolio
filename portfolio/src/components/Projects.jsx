31e9a96b7bbf19ce332606e06122320bcb7ac780
Anees A L <aneesal874@gmail.com>
2026-01-28 11:23:54 +0530

    small fixes

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
+import busbooking from  '../assets/busbooking.jpg'
+import billing from '../assets/billing.jpg'
+import doctorbooking from '../assets/doctorbooking.jpg'
 const Projects = () => {
 
 
@@ -8,21 +11,21 @@ const Projects = () => {
 
 
     const projects =[
-        {id:1,name:'Pharmacy Website',description:'An all-in-one digital pharmacy for ordering medicines online and scheduling doctor appointments seamlessly.',link:'https://github.com/Anees-al/pharmacy-app'},
-        {id:2,name:'Mern Authentication',description:'A secure MERN-based authentication system with user registration, login, and JWT-based access control',link:'https://github.com/Anees-al/MERN-Authentication'},
-        {id:3,name:'Billing System',description:'A billing system is an application that generates invoices and manages products, where the backend allows adding and managing products and handles all billing calculations and data storage.',link:'https://github.com/Anees-al/MERN-Authentication'}
+        {id:1,name:'Bus booking website',description:'A MERN-based responsive bus booking platform with real-time seat availability and a simple, intuitive booking flow.',link:' https://anees-al.github.io/busbooking/',imglink:busbooking},
+        {id:2,name:'Doctor Booking website',description:'A responsive doctor booking website that allows users to schedule appointments through a simple and user-friendly interface.',link:' https://anees-al.github.io/busbooking/',imglink:doctorbooking},
+        {id:3,name:'Billing System',description:'A billing system is an application that generates invoices, manages products, and handles all billing calculations through a backend.',link:'https://anees-al.github.io/billing-software/',imglink:billing}
     ]
   return (
     <div className='flex flex-col p-10'>
         <h1 className='text-5xl font-bold '>My <span className='text-purple-600'>Projects</span></h1>
         <div className='flex md:flex-row flex-col justify-around gap-10 mt-10'>
          {projects.map((project)=>(
-            <motion.div className='flex flex-col w-[250px] h-[300px] border justify-between p-2 rounded-lg hover:shadow-lg border-gray-200 bg-gray-100' key={project.id} initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:0.5}}>
-              <img src="" alt="" />
+            <motion.div className='flex flex-col w-[300px] h-[360px] border justify-between p-2 rounded-lg hover:shadow-lg border-gray-200 bg-gray-100' key={project.id} initial={{y:100,opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:0.5}}>
+              <img src={project.imglink} alt="" />
               <div>
-                <p className='text-center font-bold'>{project.name}</p>
-                <p className='font semibold text-gray-800'>{project.description}</p>
-               <a href={project.link}><button className='text-sm text-white bg-purple-500 px-4 py-1 font-bold rounded-lg hover:shadow-lg mt-5 cursor-pointer'>Go to project</button></a> 
+                <p className='text-xl text-center font-bold'>{project.name}</p>
+                <p className='text-sm font semibold text-gray-800'>{project.description}</p>
+               <a href={project.link}><button className='text-sm w-[270px] text-white bg-purple-500 px-4 py-2 font-bold rounded-lg hover:shadow-lg mt-5 cursor-pointer'>Go to project</button></a> 
               </div>
             </motion.div>
          ))}
